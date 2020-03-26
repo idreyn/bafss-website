3887b62f1be78101e90900dbfaba5c37edc43f28
Ian Reynolds <idreyn@gmail.com>
2020-03-26 11:49:46 -0400

    propagate errors to express

--- server/main.js
+++ server/main.js
@@ -14,10 +14,10 @@ const staticRoot = path.normalize(path.join(__dirname, '..', 'dist'));
 const app = express();
 
 app.use(express.static(staticRoot));
-app.get('/api/responses', (_, res) => {
-    return loadAndCollateResponses().then(responses =>
-        res.status(200).json({ responses })
-    );
+app.get('/api/responses', (_, res, next) => {
+    return loadAndCollateResponses()
+        .then(responses => res.status(200).json({ responses }))
+        .catch(next);
 });
 
 app.listen(process.env.PORT || 2039);
