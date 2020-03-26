fa4f6e7d6458285db893431a45d7d1322b29aa71
Santani Teng <santani@mit.edu>
2020-03-26 14:15:47 -0700

    first meaningful push

--- client/App.js
+++ client/App.js
@@ -13,62 +13,45 @@ const App = () => {
                 <div className="header">
                     <h1>Bay Area Face Shield Supply</h1>
                 </div>
-                <h2>Overview</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nulla tellus purus, lobortis ac nunc euismod, finibus
-                    euismod nibh. Nullam nec molestie mi, at rhoncus leo. Mauris
-                    tempus, nunc quis dapibus ullamcorper, ex dolor pellentesque
-                    massa, id blandit ex enim eget tortor. Donec finibus urna
-                    finibus velit pellentesque fringilla. Duis consequat dapibus
-                    suscipit. Vestibulum ornare nisl eget felis pulvinar, eget
-                    condimentum arcu dignissim. Maecenas cursus tortor vitae
-                    diam iaculis egestas. Aliquam rhoncus iaculis ultrices.
-                </p>
-                <p>
-                    Nulla facilisi. Praesent a imperdiet dolor, vitae
-                    consectetur ipsum. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Integer venenatis massa et pretium
-                    elementum. Mauris sed convallis lorem, quis vulputate
-                    turpis. Quisque metus risus, ultrices ut erat sed, suscipit
-                    sodales purus. Nam vitae velit dapibus, convallis sapien
-                    vitae, consequat felis. Sed varius finibus velit, id maximus
-                    ex rutrum sodales. Integer tristique accumsan orci, sed
-                    molestie lectus efficitur a. Interdum et malesuada fames ac
-                    ante ipsum primis in faucibus. Aenean gravida nulla in
-                    elementum facilisis. Mauris sit amet justo ut est euismod
-                    pellentesque at eget justo.
+                    Health care workers around the Bay Area are low on protective equipment as they fight the coronavirus pandemic. Help us produce and distribute 3d-printed face shields to keep them safe!
                 </p>
                 <h2>Where help is coming from</h2>
             </div>
             <Map expanded={isMapExpanded} onExpand={setMapExpanded} />
             <div className="centered-column">
-                <h2>More stuff</h2>
+                <h3>Contributing Effort</h3>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nulla tellus purus, lobortis ac nunc euismod, finibus
-                    euismod nibh. Nullam nec molestie mi, at rhoncus leo. Mauris
-                    tempus, nunc quis dapibus ullamcorper, ex dolor pellentesque
-                    massa, id blandit ex enim eget tortor. Donec finibus urna
-                    finibus velit pellentesque fringilla. Duis consequat dapibus
-                    suscipit. Vestibulum ornare nisl eget felis pulvinar, eget
-                    condimentum arcu dignissim. Maecenas cursus tortor vitae
-                    diam iaculis egestas. Aliquam rhoncus iaculis ultrices.
+                    The map above shows (by ZIP code) where people have signed up to contribute 3d printing resources, materials, and labor.
                 </p>
                 <p>
-                    Nulla facilisi. Praesent a imperdiet dolor, vitae
-                    consectetur ipsum. Interdum et malesuada fames ac ante ipsum
-                    primis in faucibus. Integer venenatis massa et pretium
-                    elementum. Mauris sed convallis lorem, quis vulputate
-                    turpis. Quisque metus risus, ultrices ut erat sed, suscipit
-                    sodales purus. Nam vitae velit dapibus, convallis sapien
-                    vitae, consequat felis. Sed varius finibus velit, id maximus
-                    ex rutrum sodales. Integer tristique accumsan orci, sed
-                    molestie lectus efficitur a. Interdum et malesuada fames ac
-                    ante ipsum primis in faucibus. Aenean gravida nulla in
-                    elementum facilisis. Mauris sit amet justo ut est euismod
-                    pellentesque at eget justo.
+                    <strong><a href="https://forms.gle/pDxaSF5idGm15di98">CLICK HERE TO HELP</a></strong> (Your personal information will remain anonymous)
                 </p>
+                <h3>Contributing Funds</h3>
+                <p>
+                    If you aren't in a position to work on this but would like to support our efforts, contributors' time is entirely donated, and we're buying all our supplies out of pocket at local and online retail outlets. Defraying some of those costs would be enormously helpful!                    
+                </p>
+                <h4>PayPal</h4>
+                <p>  
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value="RYF9NU6X6BSL4" />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                    </form>
+                </p>
+                <h4>Venmo</h4>
+                <p>
+                    @Santani-Teng
+                </p>
+
+                <h2>FAQs/About</h2>
+                <p><strong>What’s a face shield? Is it like a mask?</strong>
+                    <br></br>
+                    A face shield is not a mask that fits snugly over the nose and mouth. It is a clear window that protects the whole face from droplets and sprays that can carry infection.
+                </p>
+
+
             </div>
         </main>
     );
