14c06778249f97d077a470a9f4ec76a3da7e924f
Ian Reynolds <idreyn@gmail.com>
2020-04-13 18:46:45 -0400

    switch to a preemptive caching mechanism for sheets

--- server/sheets.js
+++ server/sheets.js
@@ -1,6 +1,6 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-import { cache } from './util';
+import { preemptiveCache } from './util';
 
 const defaultSheetExtractor = doc => {
     const [sheet] = doc.sheetsByIndex;
@@ -12,17 +12,14 @@ export const createGoogleSheetGetter = ({
     lifetimeSeconds = 60,
     sheetExtractor = defaultSheetExtractor,
 }) => {
-    return cache(
-        async () => {
-            const spreadsheetId = getId();
-            const doc = new GoogleSpreadsheet(spreadsheetId);
-            await doc.useServiceAccountAuth({
-                client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY,
-            });
-            await doc.loadInfo();
-            return sheetExtractor(doc);
-        },
-        process.env.NODE_ENV === 'development' ? 0 : lifetimeSeconds
-    );
+    return preemptiveCache(async () => {
+        const spreadsheetId = getId();
+        const doc = new GoogleSpreadsheet(spreadsheetId);
+        await doc.useServiceAccountAuth({
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY,
+        });
+        await doc.loadInfo();
+        return sheetExtractor(doc);
+    }, lifetimeSeconds);
 };
